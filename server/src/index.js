624aa806e7ac4e2fa91f829c43b1c5878922894d
George Garcia <georgegarcia846@gmail.com>
2021-03-24 22:08:42 -0700

    Final commit of the night

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,14 @@
 const { ApolloServer } = require('apollo-server')
+const { PrismaClient } = require('@prisma/client')
+const fs = require('fs')
+const path = require('path')
 
+const prisma = new PrismaClient()
 const server = new ApolloServer({
-    
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql',
+        'utf8') // TODO: Finish this up (check 'connecting a database' from GraphQL)
+    )
 })
 
 server
